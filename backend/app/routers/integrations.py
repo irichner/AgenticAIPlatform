0bb67e02ba4d4c7515c24ded3b1110ed252ad011
Israel Richner <israel.richner@gamil.com>
2026-04-30 13:25:43 -0500

    fix(integrations): remove unused gmail.send scope from OAuth flow

--- backend/app/routers/integrations.py
+++ backend/app/routers/integrations.py
@@ -42,7 +42,6 @@ async def _get_google_integration_config(db: AsyncSession, org_id: UUID) -> tupl
 SCOPES = [
     "https://www.googleapis.com/auth/drive.readonly",
     "https://www.googleapis.com/auth/gmail.readonly",
-    "https://www.googleapis.com/auth/gmail.send",
     "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
